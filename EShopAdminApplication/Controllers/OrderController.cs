ae62510af97961c0adba66213a9bae065c1cbb4d
Nadica Dimitrovska <you@example.com>
2023-05-31 18:15:13 +0200

    Auditory Exercise 8

--- EShopAdminApplication/Controllers/OrderController.cs
+++ EShopAdminApplication/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using EShopAdminApplication.Models;
+﻿using ClosedXML.Excel;
+using EShopAdminApplication.Models;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -96,5 +97,57 @@ namespace EShopAdminApplication.Controllers
 
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
         }
+
+        [HttpGet]
+        public FileContentResult ExportAllOrders()
+        {
+            HttpClient client = new HttpClient();
+
+            string URL = "https://localhost:44356/api/Admin/GetAllActiveOrders";
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+
+            var result = response.Content.ReadAsAsync<List<Order>>().Result;
+
+            string fileName = "Orders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workBook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workBook.Worksheets.Add("All Orders");
+
+                worksheet.Cell(1, 1).Value = "Order Id";
+                worksheet.Cell(1, 2).Value = "Costumer Email";
+                //worksheet.Cell(1, 3).Value = "Costumer Last Name";
+                //worksheet.Cell(1, 4).Value = "Costumer Email";
+
+                for (int i = 1; i <= result.Count(); i++)
+                {
+                    var item = result[i - 1];
+
+                    worksheet.Cell(i + 1, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i + 1, 2).Value = item.User.Email;
+                    //worksheet.Cell(i + 1, 3).Value = item.User.LastName;
+                    //worksheet.Cell(i + 1, 4).Value = item.User.Email;
+
+                    for (int p = 0; p < item.productInOrders.Count(); p++)
+                    {
+                        worksheet.Cell(1, p + 3).Value = "Product-" + (p + 1);
+                        worksheet.Cell(i + 1, p + 3).Value = item.productInOrders.ElementAt(p).OrderedProduct.ProductName;
+                    }
+
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+
+                    var content = stream.ToArray();
+
+                    return File(content, contentType, fileName);
+                }
+            }
+        }
+
     }
 }
